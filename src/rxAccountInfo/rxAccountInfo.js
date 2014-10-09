dae06b7fe2f343616d165a17b4028c8035634884
Jay Parlar <parlar@gmail.com>
2014-10-09 15:24:55 -0400

    feat(rxAccountInfo): Starting to work

--- src/rxAccountInfo/rxAccountInfo.js
+++ src/rxAccountInfo/rxAccountInfo.js
@@ -1,8 +1,22 @@
-angular.module('encore.ui.rxAccountInfo', ['encore.ui.rxInfoPanel'])
-.directive('rxAccountInfo', function () {
+angular.module('encore.ui.rxAccountInfo', [])
+.directive('rxAccountInfo', function (SupportAccount, Encore) {
     return {
         templateUrl: 'templates/rxAccountInfo.html',
         restrict: 'E',
-        scope: {}
+        scope: {
+            accountNumber: '@'
+        },
+        link: function (scope) {
+            var success = function (badges) {
+                scope.badges = [_.cloneDeep(badges[0]), _.cloneDeep(badges[0]), _.cloneDeep(badges[0])];
+            };
+            var success2 = function (data) {
+                scope.accountName = data.name;
+            };
+            SupportAccount.getBadges({ accountNumber: scope.accountNumber }, success);
+
+            Encore.getAccount({ id: scope.accountNumber }, success2);
+            
+        }
     };
 });
